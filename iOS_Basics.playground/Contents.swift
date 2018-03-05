1f28364a74357fc9f1ac523d1c5b7c0cf3280d4e
Modesto Cabrera <cabreramod03@gmail.com>
2018-03-05 23:37:58 +0700

    Initial commit

---
+++ iOS_Basics.playground/Contents.swift
@@ -0,0 +1,84 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+// This is a single line comment in Swift
+
+
+/*
+ This is
+ a
+ multiple
+ line
+ comment in
+ Swift
+ */
+
+
+
+//VARIABLES AND CONSTANTS
+//Declaring Variables in Swift
+
+//Explicit declaration of Integer Variable with value 25 // implicit declaration
+var number1: Int = 25 // var number1 = 25
+
+//prints varible number1 to console
+print(number1) // 25
+
+//Changing Variable number1 value to 100 //applicable to all var declarations.
+number1 = 100
+
+print(number1) // 100
+
+//Declaring Floats in Swift, floats are decimal values. // implicit declaration
+var float1: Float = 1.053 // var float1 = 1.02
+
+print(float1) // 1.053
+
+//Declaring Double in Swift, Double are decimal values with more precision than floats // implicit declaration //
+var double1: Double = 1.0443
+
+print(double1) // 1.04443
+
+//Declaring String Variables Explicitly // implicit declaration
+var stringOne: String = "1" // var stringOne = "!"
+
+print(stringOne) // 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
